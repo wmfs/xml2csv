cc53af3547fcf96ac0ad50e1f4a1bf098b01ee41
baipi <pierre.bails@gmail.com>
2020-01-24 14:18:28 +0100

    Add unit tests

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
 /* eslint-env mocha */
 
 const path = require('path')
-const fs = require('fs')
+const fs = require('fs-extra')
 const expect = require('chai').expect
 const xml2csv = require('./../lib')
 
-describe('Run some basic tests', function () {
+describe('Run integration tests', function () {
   it('should convert the XML file to a CSV file with callback', function (done) {
     const outputFile = path.resolve(__dirname, 'output', 'simpsons.csv')
     const expectedFile = path.resolve(__dirname, 'expected', 'simpsons.csv')
@@ -37,13 +37,37 @@ describe('Run some basic tests', function () {
     )
   })
 
-  it('should convert the XML file to a CSV file with Promise', async function () {
+  it('should convert the XML file to a CSV file with async/await', async function () {
     const outputFile = path.resolve(__dirname, 'output', 'simpsons.csv')
     const expectedFile = path.resolve(__dirname, 'expected', 'simpsons.csv')
 
     const res = await xml2csv({
       xmlPath: path.resolve(__dirname, 'fixtures', 'simpsons.xml'),
       csvPath: outputFile,
+      rootXMLElement: 'Person',
+      headerMap: [
+        ['Name', 'name', 'string'],
+        ['Age', 'age', 'integer'],
+        ['Gender', 'gender', 'string'],
+        ['Brother', 'brother', 'string', 'Siblings'],
+        ['Sister', 'sister', 'string', 'Siblings']
+      ]
+    })
+
+    expect(res).to.deep.equal({ count: 4 })
+    const output = fs.readFileSync(outputFile, { encoding: 'utf8' }).split('\n')
+    const expected = fs.readFileSync(expectedFile, { encoding: 'utf8' }).split('\n')
+
+    expect(output).to.eql(expected)
+  })
+
+  it('should convert the XML file to a CSV file with stream', async function () {
+    const outputFile = path.resolve(__dirname, 'output', 'simpsons.csv')
+    const expectedFile = path.resolve(__dirname, 'expected', 'simpsons.csv')
+    fs.ensureFileSync(outputFile)
+    const res = await xml2csv({
+      xmlStream: fs.createReadStream(path.resolve(__dirname, 'fixtures', 'simpsons.xml')),
+      csvStream: fs.createWriteStream(outputFile),
       rootXMLElement: 'Person',
       headerMap: [
         ['Name', 'name', 'string'],
@@ -80,5 +104,99 @@ describe('Run some basic tests', function () {
     const expected = fs.readFileSync(expectedFile, { encoding: 'utf8' }).split('\n')
 
     expect(output).to.eql(expected)
+  })
+})
+
+describe('Run unit tests', () => {
+  it('Should fail if xmlStream and xmlPath options are provided', async function () {
+    try {
+      const input = path.resolve(__dirname, 'fixtures', 'simpsons.xml')
+      const output = path.resolve(__dirname, 'output', 'weirdCases.csv')
+
+      await xml2csv({
+        xmlPath: input,
+        xmlStream: fs.createReadStream(input),
+        csvPath: output,
+        rootXMLElement: 'Person',
+        headerMap: [
+          ['Name', 'name', 'string'],
+          ['Age', 'age', 'integer'],
+          ['Gender', 'gender', 'string'],
+          ['Brother', 'brother', 'string', 'Siblings'],
+          ['Sister', 'sister', 'string', 'Siblings']
+        ]
+      })
+    } catch (err) {
+      expect(err.message).to.equal('Please provide either xmlStream or xmlPath')
+      return
+    }
+    throw (new Error('Should not succeed'))
+  })
+
+  it('Should fail if none of xmlStream and xmlPath options are provided', async function () {
+    try {
+      await xml2csv({
+        csvPath: 'output.csv',
+        rootXMLElement: 'Person',
+        headerMap: [
+          ['Name', 'name', 'string'],
+          ['Age', 'age', 'integer'],
+          ['Gender', 'gender', 'string'],
+          ['Brother', 'brother', 'string', 'Siblings'],
+          ['Sister', 'sister', 'string', 'Siblings']
+        ]
+      })
+    } catch (err) {
+      expect(err.message).to.equal('Please provide either xmlStream or xmlPath')
+      return
+    }
+    throw (new Error('Should not succeed'))
+  })
+
+  it('Should fail if csvStream and csvPath options are provided', async function () {
+    try {
+      const input = path.resolve(__dirname, 'fixtures', 'simpsons.xml')
+      const output = path.resolve(__dirname, 'output', 'weirdCases.csv')
+
+      await xml2csv({
+        xmlPath: input,
+        csvPath: output,
+        csvStream: fs.createWriteStream(output),
+        rootXMLElement: 'Person',
+        headerMap: [
+          ['Name', 'name', 'string'],
+          ['Age', 'age', 'integer'],
+          ['Gender', 'gender', 'string'],
+          ['Brother', 'brother', 'string', 'Siblings'],
+          ['Sister', 'sister', 'string', 'Siblings']
+        ]
+      })
+    } catch (err) {
+      expect(err.message).to.equal('Please provide either csvStream or csvPath')
+      return
+    }
+    throw (new Error('Should not succeed'))
+  })
+
+  it('Should fail if none of csvStream and csvPath options are provided', async function () {
+    try {
+      const input = path.resolve(__dirname, 'fixtures', 'simpsons.xml')
+
+      await xml2csv({
+        xmlPath: input,
+        rootXMLElement: 'Person',
+        headerMap: [
+          ['Name', 'name', 'string'],
+          ['Age', 'age', 'integer'],
+          ['Gender', 'gender', 'string'],
+          ['Brother', 'brother', 'string', 'Siblings'],
+          ['Sister', 'sister', 'string', 'Siblings']
+        ]
+      })
+    } catch (err) {
+      expect(err.message).to.equal('Please provide either csvStream or csvPath')
+      return
+    }
+    throw (new Error('Should not succeed'))
   })
 })
